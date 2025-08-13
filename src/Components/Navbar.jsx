570d773d9afd610ba0039a758141b26c14f48bab
aldienhw <aldienhaidarwahyono@gmail.com>
2025-08-13 20:19:12 +0700

    Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,13 +4,13 @@ function Navbar() {
     return (
         <>
         <nav>
-            <img className='navicon' src="/All_About_Automotive.png" alt="icon" width={64} height={64} />
+            <img className='navicon' src="/All-About-Automotive/All_About_Automotive.png" alt="icon" width={64} height={64} />
             <h1 className='iconname'>All About Automotive</h1>
             <div className='navlinks'>
                 <a href="#home">Home</a>
-                <a href="#lamborghini"><img src="/Lamborghini.png" alt="" width={40}/></a>
-                <a href="#toyota"><img src="/Toyota.png" alt="" width={40}/></a>
-                <a href="#bmw"><img src="/BMW.png" alt="" width={40}/></a>
+                <a href="#lamborghini"><img src="/All-About-Automotive/Lamborghini.png" alt="" width={40}/></a>
+                <a href="#toyota"><img src="/All-About-Automotive/Toyota.png" alt="" width={40}/></a>
+                <a href="#bmw"><img src="/All-About-Automotive/BMW.png" alt="" width={40}/></a>
             </div>
         </nav>
         </>
