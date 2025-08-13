986d74d6948503ed90d7f7f515ffabbcf0351230
aldienhw <aldienhaidarwahyono@gmail.com>
2025-08-13 20:40:51 +0700

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
         <>
         <section id='home'>
             <div className='homelogo'>
-            <img className='navicon2' src="/All-About-Automotive/All_About_Automotive.png" alt="icon" width={180} />
+            <img className='navicon2' src="All_About_Automotive.png" alt="icon" width={180} />
             </div>
             <div className='hometext'>
                 <h1>All About Automotive</h1>
